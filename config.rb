a525cd247031ea219a538ee7b710ecc2a9ada54a
Mattias Hagberg <pestbarn@gmail.com>
2016-08-09 21:34:22 +0200

    starburst.css v 0.1

--- config.rb
+++ config.rb
@@ -13,7 +13,7 @@ sass_dir = "build"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+#line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
